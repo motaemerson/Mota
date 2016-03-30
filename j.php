64a7f996453312a35b3ed02c6c3590c2ded25b98
aluno <aluno@lab-wbs.local>
2016-03-30 08:19:27 -0300

    exerciocio exibir numero par

---
+++ j.php
@@ -0,0 +1,26 @@
+	<HTML> 
+		<head>
+		   <TITLE>REDAÇAO </TITLE> 
+	    </head>
+		   <BODY> 
+		 <p>sera que consegui nesta merda</p>
+		   </body>
+ <?php
+ $num1;
+ $num2 = 2;
+ 
+ 
+ 
+		for($num1=1;$num1<20;$num1++){
+		if(($num1 % $num2) == 0) {
+		echo"<br>numero par:" . $num1;
+
+}
+
+
+}
+
+?>
+
+ 
+ </html>		          
