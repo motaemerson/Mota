a15d41d99f086c3cd8a95f83fd11e993411df1f2
aluno <aluno@lab-wbs.local>
2016-04-13 09:22:03 -0300

    inicio do formulario

--- j.php
+++ j.php
@@ -1,26 +1,4 @@
-	<HTML> 
-		<head>
-		   <TITLE>REDAÇAO </TITLE> 
-	    </head>
-		   <BODY> 
-		 <p>sera que consegui nesta merda</p>
-		   </body>
- <?php
- $num1;
- $num2 = 2;
- 
- 
- 
-		for($num1=1;$num1<20;$num1++){
-		if(($num1 % $num2) == 0) {
-		echo"<br>numero par:" . $num1;
-
-}
-
-
-}
-
-?>
-
- 
- </html>		          
+	<!DOCTYPE html>
+	<html>
+	  <meta charset= "utf"-8></meta> 
+	    e
